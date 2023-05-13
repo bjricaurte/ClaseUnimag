26812bb9ed830c35b4d61eb373a18f6ff4b91818
Bladimiro Ricaurte <bladimirolcd@gmal.com>
2023-05-13 10:20:28 -0500

    Variables y constantes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 alert("Hola mundo");
+alert("Hola");
+alert("Mundo");
+
+//Variables
+let a = 5;
+let b = 6;
+let suma = a + b;
+
+console.log("El resultado de la suma es: "+suma);
+
+let nombre = "Bladimiro"
+let nombre_dos = nombre;
+
+nombre_dos = "José";
+
+console.log("El primer nombre: "+nombre);
+console.log("El segundo nombre: "+nombre_dos);
+
+//Notaciones
