db412d6a11aa2b471b0113e09f09742c3a0492ce
Bladimiro Ricaurte <bladimirolcd@gmal.com>
2023-05-13 11:39:25 -0500

    Clase JS

--- index.js
+++ index.js
@@ -17,4 +17,48 @@ nombre_dos = "José";
 console.log("El primer nombre: "+nombre);
 console.log("El segundo nombre: "+nombre_dos);
 
-//Notaciones
+//Notaciones de variables
+camelCase = 5;
+snake_case = 5;
+
+//Y prefiero el camelCase.
+
+//Constantes
+const PI = 3.1416;
+const COLOR = "Rojo";
+
+//Tipos de datos
+//Number
+//Infinity y NaN son valores especifico: 1/0 y texto/número respectivamente.
+let numeroA = 1/0;
+let numeroB= "casa"/10;
+
+console.log(numeroA);
+console.log(numeroB);
+
+//String
+// Se usa '',"" y ´´ (Backtiks)
+let nombreString = "Bj";
+//console.log(´Hola ${nombreString}´);
+
+//Boleano: true o false, se puede usar con comparaciones, ejemplo: 4 > 3.
+let comparacion = 4 < 3;
+console.log(comparacion);
+
+//Null: Significa nada o vacío 
+let edad = null;
+console.log(edad);
+
+//Undefined
+let cosa;
+console.log(cosa);
+
+//typeOf: devuelve un tipo de dato
+console.log(typeof(5));
+console.log(typeof(undefined));
+console.log(typeof(null));
+console.log(typeof(PI));
+
+//Se puede convertir a String
+
+
